0adc375cf906e9ca13272873be4cb2a9a88fb2a9
usausa <machi.pon@gmail.com>
2017-09-29 22:33:35 +0900

    Add project

---
+++ Smart.Reflection.CodeGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smart.Reflection.CodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
