8adce6a07e20a13610d86a80a0f30f35527f701c
usausa <machi.pon@gmail.com>
2017-12-17 19:05:06 +0900

    Add attribute

---
+++ Smart.Reflection.Generative/Reflection/Generative/ReflectionGenerativeAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Smart.Reflection.Generative
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ReflectionGenerativeAttribute : Attribute
+    {
+    }
+}
