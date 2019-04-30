26d1cf895471d3f078119bed42ff22c66ff399bc
usausa <machi.pon@gmail.com>
2019-04-30 21:25:19 +0900

    Add works project base

---
+++ Sandbox2/WorkRoslynCodeGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkRoslynCodeGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
