aab3088cdd0a326bfae020e0b5b2f0698794dd63
usausa <machi.pon@gmail.com>
2019-05-06 22:03:36 +0900

    Add work tools project

---
+++ WorkTools/WorkTools.Tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkTools.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
