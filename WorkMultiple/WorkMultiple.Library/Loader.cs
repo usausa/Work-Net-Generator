344494927e18a1bfebcc75e77b0518efb5099252
usausa <machi.pon@gmail.com>
2019-08-04 09:00:18 +0900

    Add multiple project

---
+++ WorkMultiple/WorkMultiple.Library/Loader.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WorkMultiple.Library
+{
+    public class Loader
+    {
+    }
+}
---
+++ WorkMultiple/WorkMultiple/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WorkMultiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new WorkMultiple.Library.Loader();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
