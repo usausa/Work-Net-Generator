782068bcc1c048893c5f5a2847ba238468e5786c
usausa <machi.pon@gmail.com>
2019-08-09 14:31:17 +0900

    Add build project base

---
+++ WorkBuild/WorkBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
