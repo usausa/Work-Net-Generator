3f26a1f902925e982ae0c9b5f29a30c573679fdd
usausa <machi.pon@gmail.com>
2021-06-10 19:39:09 +0900

    Add project baseline

---
+++ WorkGeneratorTest/WorkGeneratorTest/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace WorkGeneratorTest
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            // TODO
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public sealed class ViewAttribute : Attribute
+    {
+        public object Id { get; }
+
+        public ViewAttribute(object id)
+        {
+            Id = id;
+        }
+    }
+
+    public enum ViewId
+    {
+        Page1,
+        Page2,
+        PageA,
+        PageB
+    }
+
+    [View(ViewId.Page1)]
+    public class Page1
+    {
+    }
+
+    [View(ViewId.Page2)]
+    public class Page2
+    {
+    }
+
+    [View(ViewId.PageA)]
+    [View(ViewId.PageB)]
+    public class PageX
+    {
+    }
+}
