9ff5d719c7c65161f81a6e568230aa18e6a6a2c6
usausa <machi.pon@gmail.com>
2021-06-12 19:20:55 +0900

    Add work project

---
+++ WorkAnalyzerTest/WorkAnalyzer/CommentAnalyzer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace WorkAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class CommentAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            id: "CommentAnalyzer",
+            title: "CommentAnalyzer",
+            messageFormat: "MessageFormat",
+            category: "Comment",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "Description.");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // TODO
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxTreeAction(AnalyzeComment);
+        }
+
+        private void AnalyzeComment(SyntaxTreeAnalysisContext context)
+        {
+            var root = context.Tree.GetCompilationUnitRoot(context.CancellationToken);
+            var commentNodes = root.DescendantTrivia()
+                .Where(x => x.IsKind(SyntaxKind.MultiLineCommentTrivia) || x.IsKind(SyntaxKind.SingleLineCommentTrivia))
+                .ToArray(); // TODO
+
+            foreach (var node in commentNodes)
+            {
+                var commentText = "";
+                switch (node.Kind())
+                {
+                    case SyntaxKind.SingleLineCommentTrivia:
+                        commentText = node.ToString().TrimStart('/');
+                        break;
+                    case SyntaxKind.MultiLineCommentTrivia:
+                        var nodeText = node.ToString();
+
+                        commentText = nodeText.Substring(2, nodeText.Length-4);
+                        break;
+                }
+
+                if (String.IsNullOrWhiteSpace(commentText))
+                {
+                    var diagnostic = Diagnostic.Create(Rule, node.GetLocation());
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
---
+++ WorkAnalyzerTest/WorkAnalyzerTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WorkAnalyzerTest
+{
+    // あああ
+    class Program
+    {
+        // aaa
+        static void Main(string[] args)
+        {
+            /* test */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
