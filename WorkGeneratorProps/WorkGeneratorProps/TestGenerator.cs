724a7d3ca2959a67536091007edfad69aadee6b2
usausa <machi.pon@gmail.com>
2021-06-14 17:31:16 +0900

    Update work

---
+++ WorkGeneratorProps/WorkGeneratorProps/TestGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace WorkGeneratorProps
+{
+    [Generator]
+    public sealed class TestGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.RootNamespace", out var rootNamespace);
+
+            context.AddSource("Namespace.cs", $"// {rootNamespace}");
+        }
+    }
+}
---
+++ WorkGeneratorProps/WorkGeneratorTarget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkGeneratorTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
